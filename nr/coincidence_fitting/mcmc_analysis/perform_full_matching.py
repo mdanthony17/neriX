a10f9f97066087d0c04c66dd8f8b2b71aa9475a2
Matt Anthony <mda2149@columbia.edu>
2016-02-28 21:44:12 -0500

    Added new submit script that should allow for multiple submits from secondary python script

--- nr/coincidence_fitting/mcmc_analysis/perform_full_matching.py
+++ nr/coincidence_fitting/mcmc_analysis/perform_full_matching.py
@@ -9,8 +9,8 @@ import neriX_simulation_config, string
 import numpy as np
 import cPickle as pickle
 
-if len(sys.argv) != 7 and len(sys.argv) != 8:
-	print 'Use is python perform_full_matching.py <degree> <cathode> <anode> <num walkers> <num steps> <num threads> [<use fake data>: t/f]'
+if len(sys.argv) != 7 and len(sys.argv) != 10:
+	print 'Use is python perform_full_matching.py <degree> <cathode> <anode> <num walkers> <num steps> <num threads> [<use fake data>: t/f] [relative accidental rate] [num fake events]'
 	sys.exit()
 
 
@@ -22,18 +22,27 @@ numWalkers = int(sys.argv[4])
 numSteps = int(sys.argv[5])
 numThreads = int(sys.argv[6])
 
+
+
 try:
 	if string.lower(sys.argv[7]) == 't':
 		usingFakeData = True
+		relativeAccidentalRate = float(sys.argv[8])
+		numFakeEvents = int(sys.argv[9])
+	
 	else:
 		usingFakeData = False
+		relativeAccidentalRate = False
+		num_fake_events = -1
 except:
 	usingFakeData = False
+	relativeAccidentalRate = False
+	num_fake_events = -1
 
 
-full_matching_analysis = neriX_simulation_analysis.neriX_simulation_analysis(15, anodeSetting, cathodeSetting, degreeSetting, use_fake_data=usingFakeData)
+full_matching_analysis = neriX_simulation_analysis.neriX_simulation_analysis(15, anodeSetting, cathodeSetting, degreeSetting, use_fake_data=usingFakeData, assumeRelativeAccidentalRate=relativeAccidentalRate, num_fake_events=numFakeEvents)
 
-sParametersFullMatching = (('photon_yield', 10.), ('charge_yield', 8.), ('res_s1', 0.3), ('res_s2', 0.1), ('n_g1', 0), ('n_res_spe', 0), ('n_par0_tac_eff', 0), ('n_par0_pf_eff', 0), ('n_par1_pf_eff', 0), ('n_g2', 0), ('n_gas_gain_mean', 0), ('n_gas_gain_width', 0), ('n_par0_trig_eff', 0), ('n_par1_trig_eff', 0), ('n_par0_e_to_i', 0), ('n_par1_e_to_i', 0), ('n_par2_e_to_i', 0))
+sParametersFullMatching = (('photon_yield', 6.), ('charge_yield', 6.), ('res_s1', 0.25), ('res_s2', 0.1), ('n_g1', 0), ('n_res_spe', 0), ('n_par0_tac_eff', 0), ('n_par0_pf_eff', 0), ('n_par1_pf_eff', 0), ('n_g2', 0), ('n_gas_gain_mean', 0), ('n_gas_gain_width', 0), ('n_par0_trig_eff', 0), ('n_par1_trig_eff', 0), ('n_par0_e_to_i', 0), ('n_par1_e_to_i', 0), ('n_par2_e_to_i', 0))
 
 full_matching_analysis.run_mcmc('full_matching', sParametersFullMatching, numWalkers=numWalkers, numSteps=numSteps, numThreads=numThreads, gpu_compute=False)
 
--- nr/coincidence_fitting/mcmc_analysis/yeti_submit_scripts/submit_systematic_fake_data.sh
+++ nr/coincidence_fitting/mcmc_analysis/yeti_submit_scripts/submit_systematic_fake_data.sh
@@ -13,7 +13,8 @@
 #PBS -e localhost:/u/3/m/mda2149/neriX/nr/coincidence_fitting/mcmc_analysis/yeti_submit_scripts/qsub/
 
 #Command to execute Python program
-python ../perform_full_matching.py 45 1.054 4.5 128 5 16
+args=("$@")
+python ../perform_full_matching.py 45 1.054 4.5 64 5 16 t ${args[0]} ${args[1]}
 
 #End of script
 
