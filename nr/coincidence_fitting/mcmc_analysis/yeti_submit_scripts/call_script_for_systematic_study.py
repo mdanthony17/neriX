f71ccc2354a814db78cf440f794ac87994b9a155
Matt Anthony <mda2149@columbia.edu>
2016-02-28 22:19:21 -0500

    Corrected submit script for args

--- nr/coincidence_fitting/mcmc_analysis/yeti_submit_scripts/call_script_for_systematic_study.py
+++ nr/coincidence_fitting/mcmc_analysis/yeti_submit_scripts/call_script_for_systematic_study.py
@@ -7,7 +7,7 @@ lNumFakeEvents = [3000, 6000, 8000, 12000, 24000]
 
 for rate in lAssumedRelativeAccidentalRates:
 	for num in lNumFakeEvents:
-		call(['qsub', 'submit_systematic_fake_data.sh', str(rate), str(num)])
+		call(['qsub', '-v', "rate=%.2f,num=%d" % (rate, num), 'submit_systematic_fake_data.sh'])
 		print 'Submitted for %.2f and %d events.' % (rate, num)
 		time.sleep(5)
 
